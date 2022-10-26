446cf1e345f7b0f4b045327590526b7ef2ea4228
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2022-10-26 16:02:18 +0200

    avec le types/colors

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface Montres {
+    bracelet?: string;
+    ecran?: string;
+    boitier?: string;
+}
+
+export const colors = {
+    "#000000": "Noir",
+    "#ffffff": "Blanc",
+    "#0000ff": "Bleu",
+    "#700097": "Violet",
+    "#FF6B00": "Orange",
+    "#770000": "Rouge",
+    "#FAFF00": "Jaune",
+    "#FF00E5": "Rose",
+    "#00D809": "Vert",
+};
